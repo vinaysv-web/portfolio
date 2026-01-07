ee44ff28845e418aaba6ceff9fc90f1e3940e3b6
vinay sv <svv302214@gmail.com>
2026-01-07 12:12:23 +0530

    Add server.js file and dependencies for running the portfolio website

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Handle all routes by serving the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Portfolio website is running on http://localhost:${PORT}`);
+});
